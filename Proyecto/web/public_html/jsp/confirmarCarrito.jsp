85f1efbb03f20910a7005be1129197a2a3f65e91
frieser <fiti.pol@gmail.com>
2013-08-15 17:39:18 +0200

    confirmation of the basket page done

---
+++ Proyecto/web/public_html/jsp/confirmarCarrito.jsp
@@ -0,0 +1,92 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
+<%@page contentType="text/html;charset=windows-1252"%>
+<%@page import="model.Articulo" %>
+<%@page import="model.Usuario" %>
+<%@page import="java.util.List" %>
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=windows-1252"/>
+    <link rel="stylesheet" href="css/estilo.css">
+       <script src="js/confirmar.js"></script>
+    <title>confirmarCarrito</title>
+  </head>
+  <body>
+  <%
+    java.util.List articulos = (java.util.List) request.getAttribute("articulos");
+    //UsuarioBean usuario= (UsuarioBean)session.getAttribute("usuario");
+    if(articulos==null){
+        out.println("Esta vacio");
+        }
+  %>
+  
+    <h1 align="center">Pantalla de confirmacion de pedido</h1>
+    <table border="2" align="center" cellpadding="10%">
+    <thead> 
+      <tr>
+        <th bgcolor="Lime">Referencia</th>
+        <th bgcolor="Lime">Descripcion</th>
+        <th bgcolor="Lime">Unidades</th>
+        <th bgcolor="Lime">Precio</th>
+        <th bgcolor="Lime">subtotal articulo</th>
+        
+      </tr>
+    </thead> 
+    <tbody>
+    <%
+		      int i=0;
+          int precio = 0;
+          int precio2 = 0;
+					for(i = 0;i<articulos.size();i++){
+    %>
+      <tr>
+      <td>
+          <% out.println(((Articulo)articulos.get(i)).getReferencia()); %>
+      </td>
+      <td>
+	       	<% out.println(((Articulo)articulos.get(i)).getDescripcion()); %>
+			</td>
+
+			<td>
+	        <% out.println(((Articulo)articulos.get(i)).getUnidades()); %>
+			</td>
+
+			<td>
+		      <% out.println(((Articulo)articulos.get(i)).getPrecio()); %>
+      </td>
+      
+          
+      
+         
+         <td colspan="3"> <%//precio = ((Articulo)articulos.get(i)).getPrecio()*((Articulo)articulos.get(i)).getUnidades(); %><%=precio%></td>
+         <%//precio2 += ((Articulo)articulos.get(i)).getPrecio()*((Articulo)articulos.get(i)).getUnidades(); %>
+      </tr>
+      
+          <%
+            
+          	}
+            session.setAttribute("articulos", articulos);
+            //session.setAttribute("usuario", usuario);
+		  	  %>
+        <tr>
+          <td></td>
+         <td></td>
+           <td></td>
+        <td><b>Total: </b></td>
+      
+              <td > <b><%=precio2%></b></td>
+        </tr>
+          
+    </tbody>
+    </table>
+    <br/>
+    <br/>
+    <div align="center">
+      <form id="botones" method="POST" action='confirmaservlet'>
+        
+        <input type="submit"  align="left" id="confirmar" value="Confirmar pedido" />
+        <input type="reset" align="right" id="cancelar" value="Cancelar pedido""/> 
+      </form>
+    </div>
+  </body>
+</html>
--- Proyecto/web/src/servlet/ConvertirServlet.java
+++ Proyecto/web/src/servlet/ConvertirServlet.java
@@ -3,8 +3,15 @@ package servlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import java.util.ArrayList;
+import java.util.Enumeration;
+
+import java.util.List;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
+
+import model.Articulo;
 
 public class ConvertirServlet extends HttpServlet {
     private static final String CONTENT_TYPE = "text/html; charset=windows-1252";
@@ -16,26 +23,54 @@ public class ConvertirServlet extends HttpServlet {
     public void doGet(HttpServletRequest request,
                       HttpServletResponse response) throws ServletException,
                                                            IOException {
-        response.setContentType(CONTENT_TYPE);
-        PrintWriter out = response.getWriter();
-        out.println("<html>");
-        out.println("<head><title>ConvertirServlet</title></head>");
-        out.println("<body>");
-        out.println("<p>The servlet has received a GET. This is the reply.</p>");
-        out.println("</body></html>");
-        out.close();
+      doPost(request, response);
     }
 
     public void doPost(HttpServletRequest request,
                        HttpServletResponse response) throws ServletException,
                                                             IOException {
-        response.setContentType(CONTENT_TYPE);
-        PrintWriter out = response.getWriter();
-        out.println("<html>");
-        out.println("<head><title>ConvertirServlet</title></head>");
-        out.println("<body>");
-        out.println("<p>The servlet has received a POST. This is the reply.</p>");
-        out.println("</body></html>");
-        out.close();
+      HttpSession sesion = request.getSession();
+
+              List<Articulo> articulos=new ArrayList<Articulo>();
+              
+              Enumeration e=request.getParameterNames();
+              int iteraciones=0;
+              int usuarios = 0;
+              int aux = 0;
+              int i=1;
+              boolean enc=false;
+              while(e.hasMoreElements() ) {  
+                  iteraciones++;
+                  e.nextElement();
+              } 
+              
+              
+              iteraciones=(int)iteraciones/5;
+              
+              System.out.println("total prametros(3) -> "+iteraciones);
+                        
+              while(!enc)
+              {
+                  if(request.getParameter("referencia-pedido"+i)!=null)  
+                  {
+                    Articulo articulo=new Articulo();
+                    articulo.setReferencia((String)request.getParameter("referencia-pedido"+i));
+                    articulo.setDescripcion((String)request.getParameter("descripcion-pedido"+i));
+                    articulo.setUnidades(Long.parseLong(request.getParameter("unidades-pedido"+i)));
+                    articulo.setPrecio(Long.parseLong((String)request.getParameter("precio-pedido"+i)));
+                    articulos.add(articulo);
+                    aux++;
+                    if(iteraciones==aux)
+                        enc=true;
+                  }
+                  i++;
+              }
+              
+              System.out.println("VOY A LLAMAR A JSP CONFIRMAR");
+
+              request.setAttribute("articulos", articulos);
+
+              RequestDispatcher dispatcher =getServletContext().getRequestDispatcher("/jsp/confirmarCarrito.jsp");
+              dispatcher.forward(request, response);
     }
 }
